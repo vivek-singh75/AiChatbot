6845e8ee8117140e716a9d31bcfe2a61d47c2c3e
Vivek Singh <svivek00singh@gmail.com>
2026-09-23 20:04:26 +0530

    Deploy frontend to GitHub Pages

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,10 @@ const chatbotCont = require("./routes/chatbot.route");
 
 app.use(express.json());
 app.use(express.text());
-app.use(cors());
+
+app.use(cors({
+    origin: "https://vivek-singh75.github.io"
+}))
 
 app.use("/api/ai" , chatbotCont);
 
